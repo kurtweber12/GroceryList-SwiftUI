9fef220d9964fb6e2d049d66732de13be372126c
kurtweber12 <kkw1995@hotmail.com>
2023-12-20 14:25:33 -0500

    Initial Commit

---
+++ GroceryList/GroceryListApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryListApp.swift
+//  GroceryList
+//
+//  Created by Kurt Weber on 12/20/23.
+//
+
+import SwiftUI
+
+@main
+struct GroceryListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
